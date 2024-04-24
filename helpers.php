434e5d7ba539e117ba1dec5e6ae0ee4507cd73da
kiki <3216018649@qq.com>
2024-04-24 16:22:03 +0800

    使用loadPartial函数引入静态页

--- helpers.php
+++ helpers.php
@@ -3,7 +3,22 @@ function basePath($path) {
     return __DIR__ . '/' . $path;
 }
 
-function loadPartial($name){
-require basePath("views/partials/{$name}.php");}
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+if (file_exists($partialPath)) {
+    require $partialPath;
+} else {
+    echo "{$name}部分视图不存在!";
+}
+}
 
-
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)){
+    require $viewPath;
+} else {
+        echo "{$name}视图不存在!";
+}
+    }
